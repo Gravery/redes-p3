967c73c4575dc62276622777538d518429207ae7
Guilherme Calça <guilhermecalca@hotmail.com>
2022-08-05 10:52:45 -0300

    Passo 1 feito

--- ip.py
+++ ip.py
@@ -13,6 +13,8 @@ class IP:
         self.enlace.registrar_recebedor(self.__raw_recv)
         self.ignore_checksum = self.enlace.ignore_checksum
         self.meu_endereco = None
+        self.hops = []
+        self.cidr = []
 
     def __raw_recv(self, datagrama):
         dscp, ecn, identification, flags, frag_offset, ttl, proto, \
@@ -31,7 +33,23 @@ class IP:
         # TODO: Use a tabela de encaminhamento para determinar o próximo salto
         # (next_hop) a partir do endereço de destino do datagrama (dest_addr).
         # Retorne o next_hop para o dest_addr fornecido.
-        pass
+        addr, = struct.unpack('!I', str2addr(dest_addr))
+        next_hop = None
+        maior = -1
+
+        for cidr, hop in self.tabela:
+            split = cidr.split('/')
+            cidr = split[0]
+            nbits = int(split[1])
+            addr2, = struct.unpack('!I', str2addr(cidr))
+
+            if (addr2 >> 32 - nbits << 32 - nbits) == (addr >> 32 - nbits << 32 - nbits):
+                if (nbits > maior):
+                    maior = nbits
+                    next_hop = hop
+
+        return next_hop
+    
 
     def definir_endereco_host(self, meu_endereco):
         """
@@ -51,7 +69,17 @@ class IP:
         """
         # TODO: Guarde a tabela de encaminhamento. Se julgar conveniente,
         # converta-a em uma estrutura de dados mais eficiente.
-        pass
+        self.tabela = tabela
+        self.cidr = []
+        self.hops = []
+
+        for cidr, hop in (tabela):
+            if cidr not in self.cidr:
+                self.cidr.append(cidr)
+                self.hops.append(hop)
+            else:
+                self.hops[self.cidr.index(cidr)] = hop
+
 
     def registrar_recebedor(self, callback):
         """
